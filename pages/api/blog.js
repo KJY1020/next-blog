0c182d4b289da18cbec9eb1931cd8ce4397cdc8b
KJY1020 <seawater1020@gmail.com>
2022-07-13 10:52:24 +0900

    init1

---
+++ pages/api/blog.js
@@ -0,0 +1,18 @@
+const blogList = [{
+    id: 1,
+    title: 'NextJS 쓰는 법',
+    content: 'NextJS 개편하다 존나 좋아',
+    readTime: 1,
+    writer: 'KJY1020',
+},
+{
+    id: 2,
+    title: '최재웅대학로에서연극해',
+    content: '오 마이 갓 최재웅 이즈 어라이브',
+    readTime: 5,
+    writer: 'KJY1020',
+},
+]
+export default function handler(req, res) {
+    res.json(blogList)
+}
---
+++ pages/blog/[id].js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { useRouter } from 'next/router';
+
+export default function Home() {
+    const router = useRouter();
+    const { id } = router.query;
+    return (
+      <div>
+        Welcome to my blog { id }!
+      </div>
+    )
+  }
+  
